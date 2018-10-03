2edf5a656af3f03a0277ac750b327f9be0362acd
Scerelyn <Scerelyn@gmail.com>
2018-10-03 14:51:30 -0600

    Improve LibraryAllSongsListControl and move it into its own folder, Add (nonfunctional) LibraryAlbumList

--- SpectralPlayerApp/LibraryViewControls/LibraryAllSongsListControl.xaml.cs
+++ SpectralPlayerApp/LibraryViewControls/LibraryAllSongsListControl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SpectralPlayerApp
+namespace SpectralPlayerApp.LibraryViewControls
 {
     /// <summary>
     /// Interaction logic for LibraryAllSongsListControl.xaml
--- SpectralPlayerApp/MainWindow.xaml.cs
+++ SpectralPlayerApp/MainWindow.xaml.cs
@@ -24,26 +24,26 @@ namespace SpectralPlayerApp
         public MainWindow()
         {
             InitializeComponent();
-            LibraryAllSongsListControl libraryAllSongs = new LibraryAllSongsListControl();
-            LibraryDock.Children.Add(libraryAllSongs);
+
             Library l = new Library();
             // sample "songs"
-            l.SongList.Add(new Song() { Name = "jnfdkalf", Artist = "Bob", AlbumName = "Good", Year = "1983", TrackNumber = 1 });
-            l.SongList.Add(new Song() { Name = "jnggsdfm", Artist = "Steve", AlbumName = "Something", Year = "2001", TrackNumber = 1 });
-            l.SongList.Add(new Song() { Name = "klsdamfk", Artist = "Steve", AlbumName = "Something", Year = "2001", TrackNumber = 2 });
-            l.SongList.Add(new Song() { Name = "asdfklfm", Artist = "Bob", AlbumName = "Good", Year = "1983", TrackNumber = 2 });
-            l.SongList.Add(new Song() { Name = "ngkfadfs", Artist = "Joe", AlbumName = "OK", Year = "2017", TrackNumber = 1 });
-            l.SongList.Add(new Song() { Name = "mgfvjoks", Artist = "Bob", AlbumName = "Adequate", Year = "1992", TrackNumber = 1 });
-            l.SongList.Add(new Song() { Name = "kfdklmda", Artist = "Joe", AlbumName = "OK", Year = "2017", TrackNumber = 2 });
-            l.SongList.Add(new Song() { Name = "ntrdsmfd", Artist = "Bob", AlbumName = "Good", Year = "1983", TrackNumber = 3 });
-            l.SongList.Add(new Song() { Name = "klmdsaff", Artist = "Steve", AlbumName = "Something", Year = "2001", TrackNumber = 3 });
-            l.SongList.Add(new Song() { Name = "opksadfm", Artist = "Steve", AlbumName = "Something", Year = "2001", TrackNumber = 4 });
-            l.SongList.Add(new Song() { Name = "uitrsmmd", Artist = "Bob", AlbumName = "Good", Year = "1983", TrackNumber = 4 });
-            l.SongList.Add(new Song() { Name = "xzcvjior", Artist = "Joe", AlbumName = "OK", Year = "2017", TrackNumber = 3 });
-            l.SongList.Add(new Song() { Name = "nmsdafio", Artist = "Bob", AlbumName = "Adequate", Year = "1992", TrackNumber = 2 });
-            l.SongList.Add(new Song() { Name = "swrmjdsa", Artist = "Joe", AlbumName = "OK", Year = "2017", TrackNumber = 4 });
+            l.SongList.Add(new Song() { Name = "jnfdkalf", Artist = "Bob", AlbumName = "Good", Year = "1983", TrackNumber = 1, Genre = "Rock" });
+            l.SongList.Add(new Song() { Name = "jnggsdfm", Artist = "Steve", AlbumName = "Something", Year = "2001", TrackNumber = 1, Genre = "Classical" });
+            l.SongList.Add(new Song() { Name = "klsdamfk", Artist = "Steve", AlbumName = "Something", Year = "2001", TrackNumber = 2, Genre = "Classical" });
+            l.SongList.Add(new Song() { Name = "asdfklfm", Artist = "Bob", AlbumName = "Good", Year = "1983", TrackNumber = 2, Genre = "Rock" });
+            l.SongList.Add(new Song() { Name = "ngkfadfs", Artist = "Joe", AlbumName = "OK", Year = "2017", TrackNumber = 1, Genre = "Rap" });
+            l.SongList.Add(new Song() { Name = "mgfvjoks", Artist = "Bob", AlbumName = "Adequate", Year = "1992", TrackNumber = 1, Genre = "Rock" });
+            l.SongList.Add(new Song() { Name = "kfdklmda", Artist = "Joe", AlbumName = "OK", Year = "2017", TrackNumber = 2, Genre = "Rap" });
+            l.SongList.Add(new Song() { Name = "ntrdsmfd", Artist = "Bob", AlbumName = "Good", Year = "1983", TrackNumber = 3, Genre = "Rock" });
+            l.SongList.Add(new Song() { Name = "klmdsaff", Artist = "Steve", AlbumName = "Something", Year = "2001", TrackNumber = 3, Genre = "Classical" });
+            l.SongList.Add(new Song() { Name = "opksadfm", Artist = "Steve", AlbumName = "Something", Year = "2001", TrackNumber = 4, Genre = "Classical" });
+            l.SongList.Add(new Song() { Name = "uitrsmmd", Artist = "Bob", AlbumName = "Good", Year = "1983", TrackNumber = 4, Genre = "Rock" });
+            l.SongList.Add(new Song() { Name = "xzcvjior", Artist = "Joe", AlbumName = "OK", Year = "2017", TrackNumber = 3, Genre = "Rap" });
+            l.SongList.Add(new Song() { Name = "nmsdafio", Artist = "Bob", AlbumName = "Adequate", Year = "1992", TrackNumber = 2, Genre = "Rock" });
+            l.SongList.Add(new Song() { Name = "swrmjdsa", Artist = "Joe", AlbumName = "OK", Year = "2017", TrackNumber = 4, Genre = "Rap" });
 
-            libraryAllSongs.LibraryListView.ItemsSource = l.GetOrderedListByArtist();
+            AllSongsControl.LibraryListView.ItemsSource = l.GetOrderedListBySong();
+            AlbumsControl.AlbumListBox.ItemsSource = l.SongList.GroupBy( song => song.Name );
         }
     }
 }
